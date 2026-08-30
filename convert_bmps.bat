bb7c31fef64057cbe0baf6b5393a9730b6ab8261
Nix <thebyt3zcode@yahoo.com>
2026-08-30 20:56:49 -0300

    reset

--- convert_bmps.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0\tools\convert_bmps.ps1" -Dir "%~dp0\assets\sprites"
-pause
--- run.bat
+++
@@ -1,1 +0,0 @@
-qemu-system-i386 -vga std -cdrom build\AetherSystemSoftware.iso -boot d -serial file:serial.log -monitor tcp:127.0.0.1:5572,server,nowait -no-reboot
